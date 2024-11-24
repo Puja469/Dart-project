6a8eb392793fd163bb988e1259735fc98ad17876
puja <poojapurbey469@gmail.com>
2024-11-24 21:49:07 +0545

    Inheritance

---
+++ innheritance.dart
@@ -0,0 +1,17 @@
+class Vehicle {
+  void move() {
+    print('Vehicle is moving');
+  }
+}
+
+class Car extends Vehicle {
+  @override
+  void move() {
+    print('Car is driving');
+  }
+}
+
+void main() {
+  var car = Car();
+  car.move(); 
+}
